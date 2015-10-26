5e7231cd828191f0580983219cb3f42227c9b2ad
amber <amberlburroughs>
2015-10-25 19:42:08 -0700

    add exit button to overlay

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,22 +1,33 @@
 // problem: poor user experience when clicking on image.
 // solution: create lightbox for lg img, keep user on page.
 var $overlay = $('<div id="overlay"></div>');
-var $image = $("<img>");
+var $image = $('<img>');
+var $dismissButton = $('<button id="exit">X</button>');
 
 //Add An image to overlay
 $overlay.append($image);
 
+//Add a exit button to overlay
+$overlay.append($dismissButton);
+
+
+//When overlay button clicked
+$dismissButton.click(function(){
+  //Hide the overlay
+  $overlay.hide(300);
+});
+
 //Add overlay
-$("body").append($overlay);
+$('body').append($overlay);
 
  //A caption to overlay
 
 //Capture the click event on a link to an image
-  $("#imgGallery a").click(function(event){
+  $('#imgGallery a').click(function(event){
     event.preventDefault();
-    var imageLocation = $(this).attr("href");
+    var imageLocation = $(this).attr('href');
       //Update overlay with the image linked in the link
-    $image.attr("src", imageLocation);
+    $image.attr('src', imageLocation);
 
      //Show the overlay.
     $overlay.show();
@@ -25,6 +36,3 @@ $("body").append($overlay);
 
   });
 
-
-//When overlay is clicked
-  //Hide the overlay
