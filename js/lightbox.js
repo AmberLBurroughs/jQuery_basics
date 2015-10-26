4fa0af5428b726a6a39dada163f56f40f1e00133
amber <amberlburroughs>
2015-10-25 18:43:40 -0700

    add image to overlay

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,21 +1,30 @@
 // problem: poor user experience when clicking on image.
 // solution: create lightbox for lg img, keep user on page.
+var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
 
-var $overlay = $("<div id="overlay"></div>");
+//Add An image to overlay
+$overlay.append($image);
 
 //Add overlay
 $("body").append($overlay);
-  //An image to overlay
+
  //A caption to overlay
 
 //Capture the click event on a link to an image
   $("#imgGallery a").click(function(event){
     event.preventDefault();
-    var href = $(this).attr("href");
-    console.log(href);
-  //Update overlay with the image linked in the link
-  //Show the overlay.
-  //Get child's alt attribute and set caption
+    var imageLocation = $(this).attr("href");
+      //Update overlay with the image linked in the link
+    $image.attr("src", imageLocation);
+
+     //Show the overlay.
+    $overlay.show();
+
+    //Get child's alt attribute and set caption
+
   });
+
+
 //When overlay is clicked
   //Hide the overlay
