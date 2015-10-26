4e83755d6fe2e540072c6a223dd159d3b7f11c5c
amber <amberlburroughs>
2015-10-25 17:38:37 -0700

    write psuedocode

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,3 +1,14 @@
 // problem: poor user experience when clicking on image.
 // solution: create lightbox for lg img, keep user on page.
 
+//An image to overlay
+//A caption to overlay
+//Add overlay
+
+//Capture the click event on a link to an image
+  //Update overlay with the image linked in the link
+  //Show the overlay.
+  //Get child's alt attribute and set caption
+
+//When overlay is clicked
+  //Hide the overlay
