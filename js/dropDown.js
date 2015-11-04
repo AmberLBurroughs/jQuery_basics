be7be82732979c99bf51e0b7da36634f21cacbbd
amber <amberlburroughs>
2015-11-04 13:05:08 -0800

    create a plan / psuedo code

---
+++ js/dropDown.js
@@ -0,0 +1,14 @@
+// PROBLEM: the menu is not responsive
+// SOLUTION: toggle navigation for smaller screens
+// create a select and append to #menu
+// circle over each menu link
+  // create an option
+  // option value is the href
+  // options text is txt of link
+  // append option to select
+// create a button
+//bind click to button
+  //go to select location on click
+// modify css to hide links & show select with button (sml screen)
+  // hides select & button (LG screen)
+// selected option depending on current page
