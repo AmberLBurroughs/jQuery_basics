d8ac43fa40b9e14246577a5b85333b06d2f1a4e4
amber <amberlburroughs>
2015-11-04 13:51:53 -0800

    creat a select menu with option

--- js/dropDown.js
+++ js/dropDown.js
@@ -1,11 +1,22 @@
 // PROBLEM: the menu is not responsive
 // SOLUTION: toggle navigation for smaller screens
+
 // create a select and append to #menu
+var $select = S("<select></select>");
+$("#menu").append($select);
 // circle over each menu link
+$("#menu a ").each(function(){
+  var $anchor = $(this);
   // create an option
+  var $option = $("<option></option>");
   // option value is the href
+  $option.val($anchor.attr("href"));
   // options text is txt of link
+  $option.text($anchor.text());
   // append option to select
+  $select.append($option);
+});
+
 // create a button
 //bind click to button
   //go to select location on click
