d68ce51bf52480895ccb1f260f770b684f511628
amber <amberlburroughs>
2015-11-04 15:52:37 -0800

    add a button

--- js/dropDown.js
+++ js/dropDown.js
@@ -2,13 +2,14 @@
 // SOLUTION: toggle navigation for smaller screens
 
 // create a select and append to #menu
-var $select = S("<select></select>");
+var $select = $("<select></select>");
 $("#menu").append($select);
 // circle over each menu link
 $("#menu a ").each(function(){
   var $anchor = $(this);
   // create an option
   var $option = $("<option></option>");
+
   // option value is the href
   $option.val($anchor.attr("href"));
   // options text is txt of link
@@ -18,8 +19,14 @@ $("#menu a ").each(function(){
 });
 
 // create a button
+var $button = $("<button>Go</button>");
+$("#menu").append($button);
 //bind click to button
+$button.click(function(){
   //go to select location on click
+  window.location = $select.val();
+});
+
 // modify css to hide links & show select with button (sml screen)
   // hides select & button (LG screen)
 // selected option depending on current page
