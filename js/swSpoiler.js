d75837612cf7639b816c30d9b0cf6376e5af2ce6
amber <amberlburroughs>
2015-11-04 11:47:42 -0800

    create work space for interactivity and responsivness to a drop down menu

--- js/swSpoiler.js
+++ js/swSpoiler.js
@@ -1,15 +1,14 @@
-//prevent spoilerphobes from seeing spoilers
-//solution: hide spoilers & reveal them through user interaction
+//Prevent spoilerphobes from seeing spoilers
+//Solution: Hide spoilers and reveal them through user interaction
 
-// - Hide spoiler
-  $(".spoiler span").hide();
-// - add a button / "mask"
-  $(".spoiler").append("<button>Reveal Spoiler!</button>");
+//1, Hide spoiler
+$(".spoiler span").hide();
+//2, Add a button
+$(".spoiler").append("<button>Reveal Spoiler!</button>");
 //3, When button pressed
-  $("button").click(function(){
-    // - when button clicked reveal spoiler
-    $("this").prev().show();
-   // - when clicked hide button
-    $(this).remove();
-  });
-
+$("button").click(function(){
+  //3.1, Show spoiler next to the button clicked
+  $(this).prev().show();
+  //3.2, Get rid of button
+  $(this).remove();
+});
