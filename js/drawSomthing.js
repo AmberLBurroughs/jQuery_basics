d707545ee24e544f097b9135e8bdbadea4ba2a50
amber <amberlburroughs>
2015-11-10 14:44:43 -0800

    create pseudo code and plans for adding interactivity to drawing project

---
+++ js/drawSomthing.js
@@ -0,0 +1,20 @@
+// no user interaction, no change to canvas
+// when user interacts with buttons and canvas cause events to happen
+
+// when clicking on control li
+  // deselect sibling elements
+  // select clicked elements
+
+
+// when clicking new color
+  // show or hide color select
+
+// when color range changed
+  // update the new color span
+
+// when add color is clicked
+  // append color to controls ul
+  // select new color that is added
+
+// on mouse even on canvas
+  // draw lines
