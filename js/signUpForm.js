239c220f87dec6deb4124d51eb31609dcf2d2e52
amber <amberlburroughs>
2015-11-10 12:07:13 -0800

    create psudo code for form project

---
+++ js/signUpForm.js
@@ -0,0 +1,15 @@
+// hints are being shown when always even when valid
+// toggle hints at appropriate times
+
+//hide hints
+
+
+// event listener on PW input
+// test if valid
+  // if true hide hints
+    // else show hints
+
+// event listener on confirmation input
+// test if valid (match w/ PW input)
+  // if true hide hints
+    // else show hints
