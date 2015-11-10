221f05689c042c34f261f2d30bc15449d78cb2b8
amber <amberlburroughs>
2015-11-10 12:08:57 -0800

    execute plan

--- js/signUpForm.js
+++ js/signUpForm.js
@@ -2,14 +2,23 @@
 // toggle hints at appropriate times
 
 //hide hints
-
+$("form span").hide();
 
 // event listener on PW input
-// test if valid
-  // if true hide hints
-    // else show hints
+$("#password").focus(function(){
+ // test if valid
+   if( $(this).val().length > 8){
+    // if true hide hints
+
+
+   }else{
+   // else show hints
+
+   }
+});
 
 // event listener on confirmation input
-// test if valid (match w/ PW input)
+$("#confirm_password").
+  // test if valid (match w/ PW input)
   // if true hide hints
     // else show hints
