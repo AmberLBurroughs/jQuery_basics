24535f2727e7b14e0983c0d5681af81f7eb23a85
amber <amberlburroughs>
2015-11-10 12:32:37 -0800

    create a method to test pw input is valid , show/ hide accordingly

--- js/signUpForm.js
+++ js/signUpForm.js
@@ -4,21 +4,23 @@
 //hide hints
 $("form span").hide();
 
+function passwordEvent(){
+   // test if valid
+  if($(this).val().length > 8) {
+     // if true hide hints
+    $(this).next().hide();
+  } else{
+     // else show hints
+    $(this).next().show();
+  }
+}
+
+
 // event listener on PW input
-$("#password").focus(function(){
- // test if valid
-   if( $(this).val().length > 8){
-    // if true hide hints
-
-
-   }else{
-   // else show hints
-
-   }
-});
+$("#password").focus(passwordEvent).keyup(passwordEvent);
 
 // event listener on confirmation input
-$("#confirm_password").
+
   // test if valid (match w/ PW input)
   // if true hide hints
     // else show hints
